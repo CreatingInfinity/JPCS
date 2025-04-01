1e323978f2d3a097beb737889cca7c57233d59db
CreatingInfinity <83005369+CreatingInfinity@users.noreply.github.com>
2025-04-01 06:56:52 +0000

    app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
-<<<<<<< HEAD
-=======
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
->>>>>>> a514beeccbba13601a52edfe527b875f60f29abf
 
 function App() {
 
