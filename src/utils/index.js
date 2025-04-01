e02e21dd9081a11955a2966ebe14ec4e3101aa20
CreatingInfinity <axelleltanal@gmail.com>
2025-04-01 16:55:58 +0800

    images and carousel

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,6 +3,11 @@ import uae from "/assets/images/united-arab-emirates.png";
 import us from "/assets/images/flags.png";
 import france from "/assets/images/france.png";
 import china from "/assets/images/china.png";
+import business from "/assets/images/business.jpg";
+import ITService from "/assets/images/itservices.jpg";
+import marketing from "/assets/images/marketing.jpg";
+import employment from "/assets/images/employment.jpg";
+
 
 import hero from "/assets/videos/bg-video.mp4";
 export const heroVideo = hero;
@@ -12,3 +17,8 @@ export const uaeFlag = uae;
 export const usFlag = us;
 export const franceFlag = france;
 export const chinaFlag = china;
+
+export const businessImage = business;
+export const ITServiceImage = ITService;
+export const marketingImage = marketing;
+export const employmentImage = employment;
