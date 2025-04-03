9051ec8616b225f71491a891e41ba39dfa7d0b35
CreatingInfinity <axelleltanal@gmail.com>
2025-04-03 20:11:44 +0800

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import NavBar from './components/NavBar'
-import Hero from './components/Hero'
-import Highlights from './components/Highlights'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import NavBar from './components/NavBar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
 
-
-function App() {
-
+const App = () => {
   return (
-    <>
-      <main>
-        <nav className='poppins-medium '>
-          <NavBar />
-        </nav>
-        <Hero />
-        <Highlights />
-      </main>
-    </>
-  )
-}
+    <Router>
+      <nav className='poppins-medium '>
+        <NavBar />
+      </nav>
+      <Routes>
+        <Route path="/" element={<Hero />} />
+      </Routes>
+      <Highlights />
+    </Router>
+  );
+};
 
-export default App
+export default App;
