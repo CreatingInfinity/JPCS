f1c115ee6cb2a9b997ebdf3cf1cc3504bb49933b
CreatingInfinity <axelleltanal@gmail.com>
2025-04-10 20:18:40 +0800

    changes april 10

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const menuRef = useRef(null);
+
   return (
     <Router>
       <nav className='poppins-medium'>
         <NavBar />
       </nav>
       <Routes>
-        <Route path="/" element={<Hero />} />
+        <Route path="/" element={<Hero menuRef={menuRef} />} />
       </Routes>
     </Router>
   );
